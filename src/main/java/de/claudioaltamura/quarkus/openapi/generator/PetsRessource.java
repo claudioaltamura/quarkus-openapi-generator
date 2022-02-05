98f8b0fd3411d300f12f5544062408095cce1d9f
Claudio Altamura <claudio@claudioaltamura.de>
2022-02-05 21:42:40 +0100

    chg: changed to response return type

--- src/main/java/de/claudioaltamura/quarkus/openapi/generator/PetsRessource.java
+++ src/main/java/de/claudioaltamura/quarkus/openapi/generator/PetsRessource.java
@@ -2,36 +2,57 @@ package de.claudioaltamura.quarkus.openapi.generator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import de.claudioaltamura.quarkus.openapi.generator.api.PetsApi;
 import de.claudioaltamura.quarkus.openapi.generator.model.Pet;
 
 public class PetsRessource implements PetsApi {
 
-	@Override
-	public void createPets() {
+	@Context
+	private UriInfo uriInfo;
 
-	}
+	private List<Pet> pets = new ArrayList<>();
 
-	@Override
-	public List<Pet> listPets(Integer limit) {
-		List<Pet> pets = new ArrayList<>();
+	public PetsRessource() {
 		Pet pet = new Pet();
 		pet.setId(1L);
 		pet.setName("Flush");
 		pet.setTag("Flush");
 		pets.add(pet);
-
-		return pets;
 	}
 
 	@Override
-	public Pet showPetById(String petId) {
+	public Response createPets() {
+		Pet pet = new Pet();
+		long nextId = ThreadLocalRandom.current().nextLong(1000);
+		pet.setId(nextId);
+		pet.setName("Pet " + nextId);
+		pet.setTag("Pet Tag " + nextId);
+		pets.add(pet);
+
+		UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+		uriBuilder.path(Long.toString(nextId));
+		return Response.created(uriBuilder.build()).entity(pet).build();
+	}
+
+	@Override
+	public Response listPets(Integer limit) {
+		return Response.ok(pets).build();
+	}
+
+	@Override
+	public Response showPetById(String petId) {
 		Pet pet = new Pet();
 		pet.setId(1L);
 		pet.setName("Flush");
 		pet.setTag("Flush");
 
-		return pet;
+		return Response.ok(pet).build();
 	}
 }
